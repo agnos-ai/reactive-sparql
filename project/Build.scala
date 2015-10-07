bf9b056871cc43785f50f5b06c163dab0a799a3a
Mayworm <xbbl2ya@ml07dtsg1503740.ams.bnymellon.net>
2015-10-07 16:58:50 -0400

    minor changes on build configuration

---
+++ project/Build.scala
@@ -0,0 +1,75 @@
+import sbt._
+import Keys._
+import spray.revolver.RevolverPlugin._
+
+
+object BuildSettings {
+  val buildOrganization = "com.briskware"
+  val buildVersion      = "0.0.1-SNAPSHOT"
+  val buildScalaVersion = Version.scala
+  val buildExportJars   = true
+
+  val buildSettings = Seq (
+    organization  := buildOrganization,
+    version       := buildVersion,
+    scalaVersion  := buildScalaVersion,
+    exportJars    := buildExportJars,
+    shellPrompt := { state => "sbt [%s]> ".format(Project.extract(state).currentProject.id) },
+    scalacOptions := Seq("-deprecation", "-unchecked", "-feature", "-target:jvm-1.7", "-language:implicitConversions", "-language:postfixOps", "-Xlint", "-Xfatal-warnings"),
+    incOptions    := incOptions.value.withNameHashing(nameHashing     = true),
+    ivyScala      := ivyScala.value map { _.copy(overrideScalaVersion = true) },
+    parallelExecution in Test := true
+  ) ++ Defaults.itSettings ++ Revolver.settings
+}
+
+object Version {
+
+  val scala      = "2.11.7"
+  val akka       = "2.3.13"
+  val sprayJson  = "1.3.2"
+  val spray      = "1.3.3"
+  val javaxWsRs  = "1.1.1"
+  val jersey  = "1.19"
+  val logback              = "1.1.2"
+  val scalaTest            = "2.2.5"
+}
+
+object Library {
+
+
+//libraryDependencies += "com.modelfabric" %% "scala-utils" % "0.1-SNAPSHOT"
+  val akkaActor            = "com.typesafe.akka"       %% "akka-actor"                  % Version.akka
+  val akkaSlf4j            = "com.typesafe.akka"       %% "akka-slf4j"                  % Version.akka
+  val akkaTestkit          = "com.typesafe.akka"       %% "akka-testkit"                % Version.akka
+
+  val sprayJson            = "io.spray"                % "spray-json_2.11"              % Version.sprayJson
+  val sprayClient          = "io.spray"                %% "spray-client"                % Version.spray
+
+  val javaxWsRs            = "javax.ws.rs"             % "jsr311-api"                   % Version.javaxWsRs
+
+  val jerseyCore           = "com.sun.jersey"          % "jersey-core"                  % Version.jersey
+  val jerseyClient         = "com.sun.jersey"          % "jersey-client"                % Version.jersey
+
+  val logbackClassic       = "ch.qos.logback"          %  "logback-classic"             % Version.logback
+
+  val scalaTest            = "org.scalatest"           %% "scalatest"                   % Version.scalaTest  % "it,test"
+
+
+}
+
+object Build extends sbt.Build {
+
+  import BuildSettings._
+  import Library._
+  import plugins._
+
+  val projectDependencies = Seq(akkaActor, akkaSlf4j, akkaTestkit, sprayClient, sprayJson, javaxWsRs, jerseyCore, jerseyClient, logbackClassic, scalaTest)
+
+  lazy val project = Project("sparql-client", file("."))
+    .configs(IntegrationTest)
+    .settings(buildSettings: _*)
+    .settings(name := "sparql-client")
+    .settings(libraryDependencies ++= projectDependencies)
+} 
+
+
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,10 +1,11 @@
 // Comment to get more information during initialization
 logLevel := Level.Info
 
-resolvers += "spray repo" at "http://repo.spray.io"
+resolvers ++= Seq("spray repo" at "http://repo.spray.io")
 
-addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "0.3.2")
+addSbtPlugin("io.spray" % "sbt-revolver" % "0.7.2")
+
+addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "0.7.0")
 
 addSbtPlugin("com.typesafe.sbt" % "sbt-scalariform" % "1.2.0")
-
 

ff33fa7dfc1e51f6da53a231c24afb0050c8643b
Stefan Szaniszlo <stefan@szaniszlo.info>
2016-07-07 18:09:55 +0100

    add missed file

---
+++ src/main/scala/com/modelfabric/sparql/api/SparqlClientProtocol.scala
@@ -0,0 +1,50 @@
+package com.modelfabric.sparql.api
+
+sealed trait SparqlClientProtocol
+
+/**
+  * Request
+  */
+trait Request extends SparqlClientProtocol
+
+/**
+  * Represents a single Sparql request to be sent to the triple store.
+  * @param statement
+  */
+case class SparqlRequest(statement: SparqlStatement) extends Request
+
+/**
+  * "Is-Alive" request
+  */
+case object PingRequest extends Request
+
+/**
+  * Response
+  */
+trait Response extends SparqlClientProtocol
+
+/**
+  * Represents a response from the triple store for a Sparql request.
+  *
+  * @param success
+  * @param resultSet
+  * @param error
+  */
+case class SparqlResponse(
+  success: Boolean = true,
+  resultSet: Option[ResultSet] = None,
+  error: Option[SparqlClientError] = None) extends Response
+
+/**
+  * "Is-Alive" response
+  * @param success
+  */
+case class PingResponse(success: Boolean = true) extends Response
+
+
+/**
+  * Error messages
+  */
+sealed trait SparqlClientError
+case class SparqlClientRequestFailed(message: String) extends RuntimeException(message) with SparqlClientError
+case class SparqlClientRequestFailedWithError(message: String, throwable: Throwable) extends RuntimeException(message, throwable) with SparqlClientError

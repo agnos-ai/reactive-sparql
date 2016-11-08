9329701cb06219878b4e37c6793d47492ec502fc
Stefan Szaniszlo <stefan@szaniszlo.info>
2016-11-08 18:32:51 +0000

    make  repositories variables optional for non-release build

--- project/Build.scala
+++ project/Build.scala
@@ -2,6 +2,8 @@ import sbt._
 import Keys._
 import spray.revolver.RevolverPlugin._
 
+import scala.util.Try
+
 
 object BuildSettings {
   val buildOrganization = "com.modelfabric"
@@ -20,17 +22,22 @@ object BuildSettings {
   ) ++ Defaults.itSettings ++ Revolver.settings
 }
 
+
+
 object PublishingSettings {
 
-  lazy val jenkinsMavenSettings = Seq(
+  val snapshotRepository = Try("snapshots" at sys.env("REPOSITORY_SNAPSHOTS")).toOption
+  val releaseRepository =  Try("releases"  at sys.env("REPOSITORY_RELEASES" )).toOption
+
+  val jenkinsMavenSettings = Seq(
     publishMavenStyle       := true,
     publishArtifact in Test := false,
     pomIncludeRepository    := { _ => false },
     publishTo := {
       if (isSnapshot.value) {
-        Some("snapshots" at sys.env("REPOSITORY_SNAPSHOTS"))
+        snapshotRepository
       } else {
-        Some("releases" at sys.env("REPOSITORY_RELEASES"))
+        releaseRepository
       }
     },
     pomExtra := {
@@ -38,23 +45,23 @@ object PublishingSettings {
         <url>https://github.com/modelfabric/reactive-sparql.git</url>
         <connection>scm:git:https://github.com/modelfabric/reactive-sparql.git</connection>
       </scm>
-        <developers>
-          <developer>
-            <id>jgeluk</id>
-            <name>Jacobus Geluk</name>
-            <url>https://github.com/orgs/modelfabric/people/jgeluk</url>
-          </developer>
-          <developer>
-            <id>JianChen123</id>
-            <name>Jian Chen</name>
-            <url>https://github.com/orgs/modelfabric/people/JianChen123</url>
-          </developer>
-          <developer>
-            <id>szaniszlo</id>
-            <name>Stefan Szaniszlo</name>
-            <url>https://github.com/orgs/modelfabric/people/szaniszlo</url>
-          </developer>
-        </developers>
+      <developers>
+        <developer>
+          <id>jgeluk</id>
+          <name>Jacobus Geluk</name>
+          <url>https://github.com/orgs/modelfabric/people/jgeluk</url>
+        </developer>
+        <developer>
+          <id>JianChen123</id>
+          <name>Jian Chen</name>
+          <url>https://github.com/orgs/modelfabric/people/JianChen123</url>
+        </developer>
+        <developer>
+          <id>szaniszlo</id>
+          <name>Stefan Szaniszlo</name>
+          <url>https://github.com/orgs/modelfabric/people/szaniszlo</url>
+        </developer>
+      </developers>
     }
   )
 }

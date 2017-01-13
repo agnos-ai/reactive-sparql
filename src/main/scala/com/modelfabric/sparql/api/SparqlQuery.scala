436464faa67be25323bdd71d3f24f76e411177ed
Stefan Szaniszlo <stefan@szaniszlo.info>
2017-01-13 14:33:39 +0000

    override the HTTP method for queries larger that a defined threshold

--- src/main/scala/com/modelfabric/sparql/api/SparqlQuery.scala
+++ src/main/scala/com/modelfabric/sparql/api/SparqlQuery.scala
@@ -1,9 +1,41 @@
 package com.modelfabric.sparql.api
 
-import com.modelfabric.sparql.api.HttpMethod.GET
+import com.modelfabric.sparql.api.HttpMethod.{GET, POST}
 
 
 object SparqlQuery {
+
+  /**
+    * The threshold at which the SPARQL client will force to use HTTP [[POST]] method on the
+    * Sparql endpoint. This is due to HTTP limiting the size of the query string.
+    *
+    * Defaults to 512, but may be override by setting the SPARQL_CLIENT_MAX_HTTP_QUERY_URI_LENGTH
+    *
+    * @see [[https://www.w3.org/Protocols/rfc2616/rfc2616-sec3.html#sec3.2.1]]
+    */
+  val sparqlQueryPostThreshold: Int = sys.env.get("SPARQL_CLIENT_MAX_HTTP_QUERY_URI_LENGTH").map(_.toInt).getOrElse(512)
+
+  /**
+    * Decides which method to use for the query. Will return the desired method, unless the
+    * query is longer that the POST threshold configured by [[sparqlQueryPostThreshold]], in which case
+    * [[POST]] is returned
+    * @param sparql the sparql statement
+    * @param desiredMethod the desired HTTP method to use, defaults to [[GET]]
+    * @return the decided HTTP method to be used
+    */
+  def decideQueryHttpMethod(sparql: String, desiredMethod: HttpMethod): HttpMethod = {
+    (sparql, desiredMethod) match {
+      case (_, POST) =>
+        POST
+      case (s, m) if s.length > sparqlQueryPostThreshold =>
+        if (m == GET) {
+          // no log is configured so we just dump this to stderr
+          System.err.println(s"unable to set desired method GET due to query length = ${s.length}, using POST instead!!!")
+        }
+        POST
+      case (_, m) => m
+    }
+  }
 
   /**
     * Construct a SparqlQuery from the passed string and implicit prefix mappings with reasoning disabled by default.
@@ -14,7 +46,7 @@ object SparqlQuery {
     */
   def apply(sparql: String)(implicit _pm : PrefixMapping): SparqlQuery = {
     // JC: why not just create two constructors in SparqlQuery, and make it concrete
-    new SparqlQuery() { override val statement = build(sparql) }
+    new SparqlQuery() { override val statement: String = build(sparql) }
   }
 
   /**
@@ -35,10 +67,10 @@ object SparqlQuery {
   )(implicit _pm : PrefixMapping = PrefixMapping.none, _paging: PagingParams = PagingParams.Defaults): SparqlQuery = {
 
     new SparqlQuery() {
-      override val httpMethod = method
-      override val statement = build(sparql)
-      override val resultMapper = mapping
-      override val reasoning = reasoningEnabled
+      override val httpMethod: HttpMethod = decideQueryHttpMethod(sparql, method)
+      override val statement: String = build(sparql)
+      override val resultMapper: ResultMapper[_] = mapping
+      override val reasoning: Boolean = reasoningEnabled
       //override val paging = _paging
     }
   }
@@ -61,7 +93,11 @@ object SparqlQuery {
  */
 abstract class SparqlQuery()(implicit _pm : PrefixMapping) extends SparqlStatement()(_pm) {
 
-  def query = statement
+  import SparqlQuery._
+
+  def query: String = statement
+
+  override def httpMethod: HttpMethod = decideQueryHttpMethod(statement, GET)
 
   /**
     * @return a result mapper that is aware of how to map a result set to objects of a specific type.
@@ -72,4 +108,5 @@ abstract class SparqlQuery()(implicit _pm : PrefixMapping) extends SparqlStateme
   def reasoning = false
 
   def paging = NoPaging
+
 }

25d77221ab424c8a8a4d0f72a4988ff840a2613c
Stefan Szaniszlo <stefan@briskware.com>
2023-09-18 21:22:14 +0100

    minor housekeeping, making this run with scalac 3.3.1 and sbt 1.9.4

--- build.sbt
+++ build.sbt
@@ -8,25 +8,37 @@ import Dependencies._
 
 val buildOrganization = "ai.agnos"
 val buildScalaVersion = Version.scala
-val buildExportJars   = true
+val buildExportJars = true
 
-val buildSettings = Seq (
-  organization  := buildOrganization,
-  scalaVersion  := buildScalaVersion,
-  exportJars    := buildExportJars,
+val buildSettings = Seq(
+  organization := buildOrganization,
+  scalaVersion := buildScalaVersion,
+  exportJars := buildExportJars,
   updateOptions := updateOptions.value.withCachedResolution(true),
-  shellPrompt   := { state => "sbt [%s]> ".format(Project.extract(state).currentProject.id) },
-  scalacOptions := Seq("-deprecation", "-unchecked", "-feature", "-target:jvm-1.8", "-language:implicitConversions", "-language:postfixOps", "-Xlint"),
+  shellPrompt := { state =>
+    "sbt [%s]> ".format(Project.extract(state).currentProject.id)
+  },
+  scalacOptions := Seq(
+    "-deprecation",
+    "-unchecked",
+    "-feature",
+    "-target:jvm-1.8",
+    "-language:implicitConversions",
+    "-language:postfixOps",
+    "-Xlint"
+  ),
+  javaOptions := Seq(
+    "--add-exports java.xml/com.sun.org.apache.xerces.internal.util=ALL-UNNAMED"
+  ),
   parallelExecution in Test := false,
   coverageFailOnMinimum := true,
-  coverageOutputHTML    := true,
-  coverageOutputXML     := true
+  coverageOutputHTML := true,
+  coverageOutputXML := true
 ) ++ Defaults.itSettings
-
 
 lazy val project = Project("reactive-sparql", file("."))
   .configs(IntegrationTest)
   .settings(buildSettings: _*)
   .settings(name := "reactive-sparql")
   .settings(libraryDependencies ++= `reactive-sparql-dependencies`)
-  .settings(coverageMinimum := 62.0D)
+  .settings(coverageMinimum := 62.0d)
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,39 +1,52 @@
 import sbt._
 
-
 object Version {
 
-  val scala      = "2.13.3"
-  val akka       = "2.5.29"
-  val akkaHttp   = "10.1.11"
-  val javaxWsRs  = "1.1.1"
-  val rdf4j      = "2.1.6" //"2.3.2"
-  val logback    = "1.2.3"
-  val scalaTest  = "3.0.8"
-  val fuseki     = "3.7.0"
-  val xmlBind    = "2.3.2"
+  val scala = "2.13.11"
+  val akka = "2.5.31"
+  val akkaHttp = "10.1.15"
+  val javaxWsRs = "1.1.1"
+  val rdf4j = "2.1.6" //"2.3.2"
+  val logback = "1.2.3"
+  val scalaTest = "3.0.8"
+  val fuseki = "3.7.0"
+  val xmlBind = "2.3.2"
 }
-
 
 object Dependencies {
 
-  val akkaActor         = "com.typesafe.akka" %% "akka-actor"                        % Version.akka
-  val akkaStream        = "com.typesafe.akka" %% "akka-stream"                       % Version.akka
-  val akkaHttpCore      = "com.typesafe.akka" %% "akka-http-core"                    % Version.akkaHttp
-  val akkaHttpSprayJson = "com.typesafe.akka" %% "akka-http-spray-json"              % Version.akkaHttp
-  val akkaSlf4j         = "com.typesafe.akka" %% "akka-slf4j"                        % Version.akka
-  val javaxWsRs         = "javax.ws.rs"       %  "jsr311-api"                        % Version.javaxWsRs
-  val logbackClassic    = "ch.qos.logback"    %  "logback-classic"                   % Version.logback
-  val rdf4jRuntime      = "org.eclipse.rdf4j" %  "rdf4j-runtime"                     % Version.rdf4j
-  val jakartaXmlBind    = "jakarta.xml.bind"  % "jakarta.xml.bind-api"               % Version.xmlBind
-  val scalaTest         = "org.scalatest"     %% "scalatest"                         % Version.scalaTest   % "it,test"
-  val akkaTestkit       = "com.typesafe.akka" %% "akka-testkit"                      % Version.akka        % "it,test"
-  val akkaStreamTestkit = "com.typesafe.akka" %% "akka-stream-testkit"               % Version.akka        % "it,test"
-  val fusekiServer      = "org.apache.jena"   %  "jena-fuseki-server"                % Version.fuseki      % "it,test"
+  val akkaActor = "com.typesafe.akka" %% "akka-actor" % Version.akka
+  val akkaStream = "com.typesafe.akka" %% "akka-stream" % Version.akka
+  val akkaHttpCore = "com.typesafe.akka" %% "akka-http-core" % Version.akkaHttp
+  val akkaHttpSprayJson =
+    "com.typesafe.akka" %% "akka-http-spray-json" % Version.akkaHttp
+  val akkaSlf4j = "com.typesafe.akka" %% "akka-slf4j" % Version.akka
+  val javaxWsRs = "javax.ws.rs" % "jsr311-api" % Version.javaxWsRs
+  val logbackClassic = "ch.qos.logback" % "logback-classic" % Version.logback
+  val rdf4jRuntime = "org.eclipse.rdf4j" % "rdf4j-runtime" % Version.rdf4j
+  val jakartaXmlBind =
+    "jakarta.xml.bind" % "jakarta.xml.bind-api" % Version.xmlBind
+  val scalaTest = "org.scalatest" %% "scalatest" % Version.scalaTest % "it,test"
+  val akkaTestkit =
+    "com.typesafe.akka" %% "akka-testkit" % Version.akka % "it,test"
+  val akkaStreamTestkit =
+    "com.typesafe.akka" %% "akka-stream-testkit" % Version.akka % "it,test"
+  val fusekiServer =
+    "org.apache.jena" % "jena-fuseki-server" % Version.fuseki % "it,test"
 
-  val `reactive-sparql-dependencies` = Seq(
-    akkaActor, akkaStream, akkaHttpCore, akkaHttpSprayJson, akkaSlf4j,
-    javaxWsRs, rdf4jRuntime,
-    logbackClassic, scalaTest, akkaTestkit, akkaStreamTestkit, fusekiServer,
-    jakartaXmlBind)
+  val `reactive-sparql-dependencies`: Seq[ModuleID] = Seq(
+    akkaActor,
+    akkaStream,
+    akkaHttpCore,
+    akkaHttpSprayJson,
+    akkaSlf4j,
+    javaxWsRs,
+    rdf4jRuntime,
+    logbackClassic,
+    scalaTest,
+    akkaTestkit,
+    akkaStreamTestkit,
+    fusekiServer,
+    jakartaXmlBind
+  )
 }
